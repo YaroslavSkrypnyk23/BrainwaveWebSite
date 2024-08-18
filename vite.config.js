651c64785ae8d04bf307686c96cc560a9df0597e
YaroslavSkrypnyk23 <yaroslavskrypnyk22@ukr.net>
2024-08-18 09:14:51 +0300

    Chenged base in vite.conf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BrainwaveWebSite/',
   plugins: [react()],
-  base: '/jsm-brainwave', // Базовий шлях для всіх ресурсів
-  
-});
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js', // Зберігає  основний JavaScript файл в кореневій папці dist
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'index.css') {
+            return 'index.css'; // Зберігає CSS файл в кореневій папці dist
+          }
+          return 'assets/[name].[ext]'; // Інші статичні файли в папці assets
+        },
+      },
+    },
+  },
+})
